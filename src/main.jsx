971f819fa8dd7e434de12727f3910a0f03232601
Oleksii Tsioma <oleksii@thewildfields.com>
2026-04-23 15:33:05 -0700

    Add version check

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const STORAGE_VERSION = '2';
+const currentVersion = localStorage.getItem('storage_version');
+
+if (currentVersion !== STORAGE_VERSION) {
+  localStorage.clear(); // or selectively remove specific keys
+  localStorage.setItem('storage_version', STORAGE_VERSION);
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
